bd3ed0b94a8b40ba74e62cc0e8917f851d01defb
unknown <1022gusl@gmail.com>
2025-11-10 21:04:11 +0900

    refactor: 드롭다운 안 보이는 현상 개선

--- src/components/Gnb/Gnb.jsx
+++ src/components/Gnb/Gnb.jsx
@@ -31,7 +31,7 @@ function Gnb() {
   };
 
   return (
-    <header className="flex items-center justify-between h-14 px-2 sm:px-6 border-b bg-white">
+    <header className="sticky top-0 z-50 flex items-center justify-between h-14 px-2 sm:px-6 border-b bg-white">
       <div className="flex items-center sm:gap-2">
         <Menu />
         <Home />
