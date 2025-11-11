10344dc7971898875e68b4c150b7f2925f38be82
unknown <1022gusl@gmail.com>
2025-11-11 18:09:01 +0900

    feat: 디테일페이지 초기세팅

---
+++ src/pages/Detail/DetailPage.jsx
@@ -0,0 +1,9 @@
+export default function DetailPage(){
+  return (
+  <main className="bg-gray-50 min-h-screen px-4 py-8 flex items-center justify-center">
+    <section className="w-full max-w-[840px] rounded-md border border-gray-200 bg-white p-8"> 
+      <div className="text-center max-w-[480px] mx-auto">
+        <h2 className="text-2xl font-semibold text-black">추천 활동</h2>
+      </div>
+    </section> 
+  </main> )}
